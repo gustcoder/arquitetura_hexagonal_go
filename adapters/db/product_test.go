61329d760985e8bf9aa5481d9168f395543b69e3
gustcoder <gustavofaramos@gmail.com>
2025-06-15 10:16:08 -0300

    Unit Testing Save (create + update)

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/codeedu/go-hexagonal/adapters/db"
+	"github.com/codeedu/go-hexagonal/application"
 	"github.com/stretchr/testify/require"
 )
 
@@ -49,4 +50,46 @@ func TestProductDb_Get(t *testing.T) {
 	require.Equal(t, "PlayStation5", product.GetName())
 	require.Equal(t, 3500.00, product.GetPrice())
 	require.Equal(t, "enabled", product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDB := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "MXF TSX 250"
+	product.Price = 33600
+	product.Status = "enabled"
+
+	productResult, err := productDB.Save(product)
+
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+}
+
+func TestProductDb_Update(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDB := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "MXF TSX 250"
+	product.Price = 33600
+	product.Status = "enabled"
+
+	productResult, err := productDB.Save(product)
+
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Price = 36000
+
+	productUpdateResult, err := productDB.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Price, productUpdateResult.GetPrice())
 }
